89d33a4f806a115948eec960ca90eac98630a565
Dillon Marquard <dmarquard4@gmail.com>
2020-01-05 22:49:51 -0800

    Update bjjnodejs.js

--- bjjnodejs.js
+++ bjjnodejs.js
@@ -9,7 +9,7 @@ var pool = mysql.createPool({
     connectionLimit : 10, // default = 10
     host            : 'localhost',
     user            : 'root',
-    password        : 'Ducksliveforever11#',
+    password        : '',
     database        : 'bjjdb'
 });
 
